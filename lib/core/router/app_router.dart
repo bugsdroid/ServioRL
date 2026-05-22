6ea117c28603814366b1b4e543a0da90fd3cdbc7
bugsdroid <medsos7@gmail.com>
2026-05-22 15:28:23 +0700

    feat(router): update routes to match UI mockup tabs

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,28 +1,26 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/home/home_shell.dart';
-import '../../features/discover/discover_screen.dart';
-import '../../features/movies/movies_screen.dart';
-import '../../features/tv/tv_screen.dart';
+import '../../features/home/home_screen.dart';
 import '../../features/downloads/downloads_screen.dart';
-import '../../features/subtitles/subtitles_screen.dart';
+import '../../features/requests/requests_screen.dart';
+import '../../features/search/search_screen.dart';
 import '../../features/settings/settings_screen.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
-    initialLocation: '/discover',
+    initialLocation: '/home',
     routes: [
       ShellRoute(
         builder: (context, state, child) => HomeShell(child: child),
         routes: [
-          GoRoute(path: '/discover',  builder: (c, s) => const DiscoverScreen()),
-          GoRoute(path: '/movies',    builder: (c, s) => const MoviesScreen()),
-          GoRoute(path: '/tv',        builder: (c, s) => const TvScreen()),
+          GoRoute(path: '/home',      builder: (c, s) => const HomeScreen()),
           GoRoute(path: '/downloads', builder: (c, s) => const DownloadsScreen()),
-          GoRoute(path: '/subtitles', builder: (c, s) => const SubtitlesScreen()),
+          GoRoute(path: '/requests',  builder: (c, s) => const RequestsScreen()),
+          GoRoute(path: '/search',    builder: (c, s) => const SearchScreen()),
+          GoRoute(path: '/settings',  builder: (c, s) => const SettingsScreen()),
         ],
       ),
-      GoRoute(path: '/settings', builder: (c, s) => const SettingsScreen()),
     ],
   );
 });
