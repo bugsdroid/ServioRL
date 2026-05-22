5f9332894525eae1d86abb6b62e9107c4629f160
bugsdroid <medsos7@gmail.com>
2026-05-22 10:43:46 +0700

    feat: add config provider with SharedPreferences persistence

---
+++ lib/core/config/config_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'app_config.dart';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError('Override in main.dart after SharedPreferences.getInstance()');
+});
+
+final appConfigProvider = StateNotifierProvider<AppConfigNotifier, AppConfig>((ref) {
+  final prefs = ref.watch(sharedPreferencesProvider);
+  return AppConfigNotifier(prefs);
+});
+
+class AppConfigNotifier extends StateNotifier<AppConfig> {
+  final SharedPreferences _prefs;
+
+  AppConfigNotifier(this._prefs) : super(_load(_prefs));
+
+  static AppConfig _load(SharedPreferences p) => AppConfig(
+        sonarrBaseUrl:        p.getString('sonarr_url')         ?? '',
+        sonarrApiKey:         p.getString('sonarr_key')         ?? '',
+        radarrBaseUrl:        p.getString('radarr_url')         ?? '',
+        radarrApiKey:         p.getString('radarr_key')         ?? '',
+        overseerrBaseUrl:     p.getString('overseerr_url')      ?? '',
+        overseerrApiKey:      p.getString('overseerr_key')      ?? '',
+        transmissionBaseUrl:  p.getString('transmission_url')   ?? '',
+        transmissionUsername: p.getString('transmission_user')  ?? '',
+        transmissionPassword: p.getString('transmission_pass')  ?? '',
+        bazarrBaseUrl:        p.getString('bazarr_url')         ?? '',
+        bazarrApiKey:         p.getString('bazarr_key')         ?? '',
+      );
+
+  Future<void> save(AppConfig config) async {
+    state = config;
+    await Future.wait([
+      _prefs.setString('sonarr_url',        config.sonarrBaseUrl),
+      _prefs.setString('sonarr_key',        config.sonarrApiKey),
+      _prefs.setString('radarr_url',        config.radarrBaseUrl),
+      _prefs.setString('radarr_key',        config.radarrApiKey),
+      _prefs.setString('overseerr_url',     config.overseerrBaseUrl),
+      _prefs.setString('overseerr_key',     config.overseerrApiKey),
+      _prefs.setString('transmission_url',  config.transmissionBaseUrl),
+      _prefs.setString('transmission_user', config.transmissionUsername),
+      _prefs.setString('transmission_pass', config.transmissionPassword),
+      _prefs.setString('bazarr_url',        config.bazarrBaseUrl),
+      _prefs.setString('bazarr_key',        config.bazarrApiKey),
+    ]);
+  }
+}
